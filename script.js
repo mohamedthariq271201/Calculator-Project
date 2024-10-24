9f5d593e79207d64a56ed60f967c838814ef66b2
mohamedthariq271201 <tharikmohamed031@gmail.com>
2024-10-24 13:27:29 +0530

    index.html added

---
+++ script.js
@@ -0,0 +1,28 @@
+const display = document.getElementById('display');
+
+
+function appendToDisplay(value)
+ {
+  if (display.value === "0") {
+    display.value = value; 
+  } else {
+    display.value += value; 
+  }
+}
+
+
+function clearDisplay()
+ {
+  display.value = "0"; 
+}
+
+
+
+function calculateResult()
+ {
+  try {
+    display.value = eval(display.value);
+  } catch (e) {
+    display.value = "Error";
+  }
+}
